407be5c272c8c31fd47fd08c6643003b81541d88
Dipanshu Kumar <dipanshuhub0411@gmail.com>
2025-06-28 16:11:00 +0530

    medimeet

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '/**', // Allows all paths under this host
+      },
+    ],
+  },
+};
 
 export default nextConfig;
